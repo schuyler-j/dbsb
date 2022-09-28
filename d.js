354927553258fe37f57235aef680c060702d1461
Jay <schu0527@flinders.edu.au>
2022-09-28 11:07:14 +0930

    now with emojis

--- d.js
+++ d.js
@@ -1,15 +1,29 @@
-const { Client, GatewayIntentBits, Discord } = require('discord.js');
-
+const { Client, GatewayIntentBits, Discord, REST, Routes } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 secret = require("./secret.js");
+const commands = [
+	{
+		name: 'test',
+		description: 'testing',
+	},
+];
 
-/*const myIntents = new Intents();
+const rest = new REST({ version :'10' }).setToken(secret.key);
 
-myIntents.add(2048);
-old app?!?!?
-*/
+(async () => {
+	try {
+		console.log('Started (/) commands.');
+
+		await rest.put(Routes.applicationCommands(secret.CLIENT_ID), { body: commands });
+
+		console.log('Reloaded');
+	}catch (error) {
+		console.error(error);
+	}
+})();
 
 const client = new Client(
-	{intents: [GatewayIntentBits.Guilds]}
+	{intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions]}
 );
 
 
@@ -17,11 +31,31 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('message', msg => {
-	if(msg.content === 'hello') {
-		msg.reply('squawk:');
+client.on('messageCreate', msg => {
+	if(msg.content === 'h') {
+		msg.reply('squawk');
+		console.log('got it');
+	}else{
+		console.log('nope');
+		msg.react('😡');
 	}
 });
 
+/*hmm>?
+client.on('typingStart', (channel, user) => {
+	console.log("hi");
+})
+*/
+
+
+client.on('interactionCreate', async interaction => {
+	if(!interaction.isChatInputCommand()) return;
+
+	if(interaction.commandName === 'test') {
+		interaction.reply('Was this a good squawk?');
+		const message = await interaction.fetchReply();
+		message.react('😄');
+	}
+});
 
 client.login(secret.key);
